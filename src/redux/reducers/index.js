0d2443a6234d690f3ea392eb4ba651fa13aaf3aa
Chamith <chameeefdo@gmail.com>
2021-09-24 12:07:48 +0530

    create react redux file structure

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import { productReducer } from "./productReducer";
+
+
+const reducres = combineReducers({
+    allProducts: productReducer,
+})
---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,23 @@
+import { ActionTypes } from "../constants/action-types";
+
+const initialState = {
+    products:[
+        {
+            id: 1,
+            title: "Chamith",
+            category: "programmer"
+
+        },
+    ]
+}
+export const productReducer = (state,{type, payload}) => {
+
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return state;
+    
+        default:
+            return state;
+    }
+
+}
